f7df39796633af3eb94f41eb3e4e91652bfcf14e
André Pinheiro <andrepdpinheiro@gmail.com>
2015-07-27 20:51:45 +0000

    Solved 'Solve me second' challenge

---
+++ Solve me second.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        int a, b;
+
+        for(int i = 0; i < t; i++) {
+            a = in.nextInt();
+            b = in.nextInt();
+            System.out.println(a + b);
+        }
+    }
+}
