a5fbf9e566f1a2bfd16b65bfad6d07f380ff526d
André Pinheiro <andrepdpinheiro@gmail.com>
2017-05-20 20:10:58 +0100

    Update RichieRich.cs

--- Cs/RichieRich.cs
+++ Cs/RichieRich.cs
@@ -19,7 +19,7 @@ namespace HackerRank
 
             for(int i = 0; i < num.Length / 2; i++)
             {
-                if(num[i] == num[num.Length - i - 1])
+                if(num[i] == num[num.Length - i - 1] && num[i] != '9')
                 {
                     int newK = currentK - 2;
                     if(newK >= repNeed)
@@ -30,12 +30,64 @@ namespace HackerRank
                     }
                 } else
                 {
+                    if(repNeed > 0) repNeed--;
+                    int toChange = 0;
+                    if (num[i] != '9')
+                        toChange++;
+                    if (num[num.Length - i - 1] != '9')
+                        toChange++;
 
+
+                    if(toChange == 1)
+                    {
+                        int newK = currentK - toChange;
+                        if(newK >= 0 && newK >= repNeed)
+                        {
+                            if (num[i] < num[num.Length - i - 1])
+                            {
+                                num[i] = num[num.Length - i - 1];
+                                currentK--;
+                            }
+                            else if (num[i] > num[num.Length - i - 1])
+                            {
+                                num[num.Length - i - 1] = num[i];
+                                currentK--;
+                            }
+                        }
+                    }
+
+
+                    if(toChange == 2)
+                    {
+                        int newK = currentK - toChange;
+                        if(newK >= 0 && newK >= repNeed)
+                        {
+                            num[i] = '9';
+                            num[num.Length - i - 1] = '9';
+                            currentK -= 2;
+                        } else if(currentK - 1 >= 0 && currentK - 1 >= repNeed)
+                        {
+                            if (num[i] < num[num.Length - i - 1])
+                            {
+                                num[i] = num[num.Length - i - 1];
+                                currentK--;
+                            }
+                            else if (num[i] > num[num.Length - i - 1])
+                            {
+                                num[num.Length - i - 1] = num[i];
+                                currentK--;
+                            }
+                        }
+                    }
                 }
+                if (currentK < 0)
+                    return "-1";
             }
 
-            Console.WriteLine(new string(num));
-            return "";
+            if(num.Length % 2 == 1 && currentK > 0)
+                num[num.Length >> 1] = '9';
+
+            return new string(num);
         }
 
         static void Main(String[] args)
