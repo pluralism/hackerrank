d3995e41ee7d735c36dbbdc287f3f19fd7fc7131
André Pinheiro <andrepdpinheiro@gmail.com>
2017-05-20 22:38:16 +0100

    Update RichieRich.cs

--- Cs/RichieRich.cs
+++ Cs/RichieRich.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Linq;
 
-
 namespace HackerRank
 {
     class Program
@@ -10,33 +9,35 @@ namespace HackerRank
         {
             int repNeed = 0;
 
-            for(int i = 0; i < num.Length / 2; i++)
+            for(int i = 0; i < (num.Length >> 1); i++)
                 if (num[i] != num[num.Length - i - 1])
                     repNeed++;
 
-
+            if (repNeed > maxK)
+                return "-1";
             int currentK = maxK;
 
-            for(int i = 0; i < num.Length / 2; i++)
+
+            for(int i = 0; i < (num.Length >> 1); i++)
             {
-                if(num[i] == num[num.Length - i - 1] && num[i] != '9')
+                if(num[i] == num[num.Length - i - 1])
                 {
-                    int newK = currentK - 2;
-                    if(newK >= repNeed)
+                    if(num[i] != '9')
                     {
-                        num[i] = '9';
-                        num[num.Length - i - 1] = '9';
-                        currentK -= 2;
+                        int newK = currentK - 2;
+                        if (newK >= repNeed && newK >= 0)
+                        {
+                            num[i] = '9';
+                            num[num.Length - i - 1] = '9';
+                            currentK = newK;
+                        }
                     }
                 } else
                 {
                     if(repNeed > 0) repNeed--;
                     int toChange = 0;
-                    if (num[i] != '9')
-                        toChange++;
-                    if (num[num.Length - i - 1] != '9')
-                        toChange++;
-
+                    if (num[i] != '9') toChange++;
+                    if (num[num.Length - i - 1] != '9') toChange++;
 
                     if(toChange == 1)
                     {
@@ -80,11 +81,9 @@ namespace HackerRank
                         }
                     }
                 }
-                if (currentK < 0)
-                    return "-1";
             }
 
-            if(num.Length % 2 == 1 && currentK > 0)
+            if((num.Length & 1) == 1 && currentK > 0)
                 num[num.Length >> 1] = '9';
 
             return new string(num);
@@ -93,11 +92,7 @@ namespace HackerRank
         static void Main(String[] args)
         {
             string[] vals = Console.ReadLine().Split(' ');
-            int K = Int32.Parse(vals[1]);
-            char[] num = Console.ReadLine().ToCharArray();
-            Console.WriteLine(Calculate(num, K));
-
-            Console.ReadLine();
+            Console.WriteLine(Calculate(Console.ReadLine().ToCharArray(), Int32.Parse(vals[1])));
         }
     }
 }
